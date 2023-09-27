0caf8ba1b0cfb62d08f8a9ef6e43d492e9236c31
John Wooten <hello@blocktransfer.dev>
2023-09-27 09:15:38 -0400

    mv

--- beta/wc-loadAcc.js
+++ beta/wc-loadAcc.js
@@ -1,74 +1,104 @@
-// Replace Ethereum libraries with Stellar.js if needed
-// Import the Stellar SDK
-const StellarSdk = require('stellar-sdk');
-
-// Update the API baseURL for Stellar Pubnet
-const server = new StellarSdk.Server('https://horizon.stellar.org'); // Use Stellar Pubnet URL
-
-// Function to get Stellar account assets
-const getAccountAssets = async (publicKey) => {
-  try {
-    const account = await server.loadAccount(publicKey);
-    const assets = account.balances.map((balance) => ({
-      asset: balance.asset_code,
-      balance: balance.balance,
-    }));
-    return assets;
-  } catch (error) {
-    console.error(error);
-    return [];
+async function loadWalletConnectAcc() {
+  walletConnect.enable()
+  
+  
+  
+  .then((accounts) => {
+  
+  
+  // get from stellar:pubnet
+  if (accounts.length > 0) {
+    const stellarAddress = accounts[0]; // The user's Stellar address
+    
+    // Now you can work with the Stellar address
+    console.log(`Stellar Pubnet address: ${stellarAddress}`);
+    
   }
-};
+  });
+  walletConnect.on("disconnect", (error, payload) => {
+    window.location.href = "https://www.blocktransfer.com/404?wc-con";
+  });
+  walletConnect.on("accountsChanged", (accounts) => {
+    window.location.href = "https://www.blocktransfer.com/404?wc-mod";
+  });
+  walletConnect.on("chainChanged", (chainId) => {
+    window.location.href = "https://www.blocktransfer.com/404?wc-xcc";
+  });
+  
 
-// Modify the WalletConnect configuration for Stellar
-const walletConnector = new WalletConnect({
-  bridge: 'https://bridge.walletconnect.org', // Use a Stellar-compatible bridge
-  qrcode: true,
-  chainId: 8001, // Use Stellar Pubnet chain ID
-});
 
-// Update event handlers to handle Stellar-specific events
-walletConnector.on('connect', function (error, payload) {
-  if (error) {
-    console.error(error);
-  } else {
-    // Handle connection
-  }
-});
 
-walletConnector.on('session_update', function (error, payload) {
-  if (error) {
-    console.error(error);
-  } else if (walletConnector.connected) {
-    // Handle session update
-  }
-});
-
-walletConnector.on('disconnect', function (error, payload) {
-  if (error) {
-    console.error(error);
-  } else {
-    // Handle disconnection
-  }
-});
-
-// Update the connect function to initiate the WalletConnect session
-const connect = function () {
-  if (!walletConnector.connected) {
-    // create new session
-    walletConnector.createSession().then(() => {
-      // get URI for QR Code modal
-      const uri = walletConnector.uri;
-      // display QR Code modal
-      WalletConnectQRCodeModal.open(uri, () => {
-        console.log('QR Code Modal closed');
+  // Replace Ethereum libraries with Stellar.js if needed
+  // Import the Stellar SDK
+  const StellarSdk = require('stellar-sdk');
+  
+  // Update the API baseURL for Stellar Pubnet
+  const server = new StellarSdk.Server('https://horizon.stellar.org'); // Use Stellar Pubnet URL
+  
+  // Function to get Stellar account assets
+  const getAccountAssets = async (publicKey) => {
+    try {
+      const account = await server.loadAccount(publicKey);
+      const assets = account.balances.map((balance) => ({
+        asset: balance.asset_code,
+        balance: balance.balance,
+      }));
+      return assets;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  };
+  
+  // Modify the WalletConnect configuration for Stellar
+  const walletConnector = new WalletConnect({
+    bridge: 'https://bridge.walletconnect.org', // Use a Stellar-compatible bridge
+    qrcode: true,
+    chainId: 8001, // Use Stellar Pubnet chain ID
+  });
+  
+  // Update event handlers to handle Stellar-specific events
+  walletConnector.on('connect', function (error, payload) {
+    if (error) {
+      console.error(error);
+    } else {
+      // Handle connection
+    }
+  });
+  
+  walletConnector.on('session_update', function (error, payload) {
+    if (error) {
+      console.error(error);
+    } else if (walletConnector.connected) {
+      // Handle session update
+    }
+  });
+  
+  walletConnector.on('disconnect', function (error, payload) {
+    if (error) {
+      console.error(error);
+    } else {
+      // Handle disconnection
+    }
+  });
+  
+  // Update the connect function to initiate the WalletConnect session
+  const connect = function () {
+    if (!walletConnector.connected) {
+      // create new session
+      walletConnector.createSession().then(() => {
+        // get URI for QR Code modal
+        const uri = walletConnector.uri;
+        // display QR Code modal
+        WalletConnectQRCodeModal.open(uri, () => {
+          console.log('QR Code Modal closed');
+        });
       });
-    });
-  } else {
-    // disconnect
-    walletConnector.killSession();
-  }
-};
-
-// Other code remains mostly the same, but make sure to update Ethereum-specific
-// parts to work with Stellar as needed.
+    } else {
+      // disconnect
+      walletConnector.killSession();
+    }
+  };
+  
+  // Other code remains mostly the same, but make sure to update Ethereum-specific
+  // parts to work with Stellar as needed.
